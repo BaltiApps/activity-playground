cdd7d63e35467c4503793875bed24b3f39ec709c
SayantanRC <bin.sayantan@gmail.com>
2025-02-06 00:59:26 +0530

    [feat] launch from MainActivity

--- app/src/main/java/baltiapps/training/lifecycleplayground/MainActivity.kt
+++ app/src/main/java/baltiapps/training/lifecycleplayground/MainActivity.kt
@@ -1,16 +1,20 @@
 package baltiapps.training.lifecycleplayground
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import baltiapps.training.lifecycleplayground.ui.screens.activityLifecycle.ActivityLifecycle
 import baltiapps.training.lifecycleplayground.ui.theme.LifecyclePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LifecyclePlaygroundTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                startActivity(
+                                    Intent(this@MainActivity, ActivityLifecycle::class.java)
+                                )
+                            }
+                        ) {
+                            Text(stringResource(R.string.activity_lifecycle))
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LifecyclePlaygroundTheme {
-        Greeting("Android")
     }
 }
