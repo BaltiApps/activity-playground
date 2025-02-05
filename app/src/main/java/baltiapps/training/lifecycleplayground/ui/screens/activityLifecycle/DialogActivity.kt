1a36d2f1c2c2b21900b7bb6692ef35030b5b8347
SayantanRC <bin.sayantan@gmail.com>
2025-02-06 03:28:53 +0530

    [refactor] use XML layout for DialogActivity

--- app/src/main/java/baltiapps/training/lifecycleplayground/ui/screens/activityLifecycle/DialogActivity.kt
+++ app/src/main/java/baltiapps/training/lifecycleplayground/ui/screens/activityLifecycle/DialogActivity.kt
@@ -2,62 +2,11 @@ package baltiapps.training.lifecycleplayground.ui.screens.activityLifecycle
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import baltiapps.training.lifecycleplayground.R
-import baltiapps.training.lifecycleplayground.ui.theme.LifecyclePlaygroundTheme
 
 class DialogActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LifecyclePlaygroundTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.android_icon),
-                        contentDescription = null,
-                    )
-                    Spacer(
-                        modifier = Modifier.size(16.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.dialog_activity_desc),
-                        fontWeight = FontWeight.Medium,
-                    )
-                    Spacer(
-                        modifier = Modifier.size(8.dp)
-                    )
-                    Text(
-                        text = stringResource(R.string.dialog_activity_desc2),
-                        style = MaterialTheme.typography.bodyMedium,
-                        fontStyle = FontStyle.Italic,
-                        textAlign = TextAlign.Center,
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.dialog_activity_layout)
     }
 }
